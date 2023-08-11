adfff2e06b9c9d4b0c2836fc4e4cc45e793b516d
agustin-digitalfemsa <138250039+agustin-digitalfemsa@users.noreply.github.com>
2023-08-11 12:44:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App'; // Comentar para utilizar example.
-//import App from './AppExample';
+import App from './AppExample'; // Comentar para utilizar domain.
+//import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
