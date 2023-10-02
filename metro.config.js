32b9f3fff734ab476d57a3936824e9f4a323f88d
Rolando Selvera <rolandoselvera@multiplica.com>
2023-10-02 14:00:10 -0500

    Fix metro config

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,4 @@
-import { getDefaultConfig, mergeConfig } from "@react-native/metro-config";
-
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config")
 const defaultConfig = getDefaultConfig(__dirname);
 const { assetExts, sourceExts } = defaultConfig.resolver;
 
@@ -19,4 +18,4 @@ const config = {
   }
 };
 
-export default mergeConfig(defaultConfig, config);
+module.exports = mergeConfig(defaultConfig, config);
