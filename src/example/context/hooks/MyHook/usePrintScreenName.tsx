b0d1bf62669bf3639e572ced29599efdfd01fc02
Jonatan Olmedo <olmedojo@MacBook-Pro-3.local>
2023-10-05 18:37:08 -0300

    Code coverage

--- src/example/context/hooks/MyHook/usePrintScreenName.tsx
+++ src/example/context/hooks/MyHook/usePrintScreenName.tsx
@@ -2,7 +2,7 @@ import { useRoute } from "@react-navigation/native";
 
 export const usePrintScreenName = () => {
   const screenName = useRoute();
-  console.log(screenName.name);
+  //console.log(screenName.name);
 
   return {
     screenName,
