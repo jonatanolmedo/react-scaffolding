8e7f00d5fdd8a5330183fd41be4a0e2a2d97aab0
Jonatan Olmedo <olmedojo@MacBook-Pro-3.local>
2023-10-06 18:34:32 -0300

    Code coverage

---
+++ src/domain/__tests__/MyContext.test.tsx
@@ -0,0 +1,35 @@
+// __tests__/mycontext.test.tsx
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import MyProvider from '../context/MyContext/MyProvider';
+import { useMyContext } from '../context/MyContext/MyContext'; // Import useMyContext directly
+import { Text, View, Button } from 'react-native';
+
+const MockComponent = () => {
+  const { name, setName } = useMyContext();
+
+  return (
+    <View>
+      <Text testID="name">{name}</Text>
+      <Button title="Set Name" onPress={() => setName('John')} testID="button" />
+    </View>
+  );
+};
+
+describe('MyContext', () => {
+  it('provides the correct values', () => {
+    const { getByTestId } = render(
+      <MyProvider>
+        <MockComponent />
+      </MyProvider>
+    );
+
+    const nameElement = getByTestId('name');
+    const buttonElement = getByTestId('button');
+
+    expect(nameElement.props.children).toBe('');
+
+    fireEvent.press(buttonElement);
+    expect(nameElement.props.children).toBe('John');
+  });
+});
---
+++ src/example/__tests__/ProfileScreen.test.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import ProfileScreen from '../components/screens/ProfileScreen/ProfileScreen'
+import { usePrintScreenName } from '../context/hooks/MyHook/usePrintScreenName';
+
+// Mocks para react-native-gesture-handler y @react-navigation/stack
+import 'react-native-gesture-handler/jestSetup';
+
+// Mock de MyProvider
+jest.mock('../context/MyContext/MyProvider', () => ({
+  MyProvider: ({ children }: { children: React.ReactNode }) => <>{children}</>,
+}));
+
+// Mock de usePrintScreenName
+jest.mock('../context/hooks/MyHook/usePrintScreenName', () => ({
+  usePrintScreenName: jest.fn().mockReturnValue('MockedScreen'),
+}));
+
+jest.mock('@react-navigation/stack', () => ({
+  ...jest.requireActual('@react-navigation/stack'),
+  createNativeStackNavigator: jest.fn(),
+}));
+
+jest.mock('react-native-gesture-handler', () => {
+  const View = require('react-native/Libraries/Components/View/View');
+  return {
+    Swipeable: View,
+    DrawerLayout: View,
+    State: {},
+    ScrollView: View,
+    Slider: View,
+    Switch: View,
+    TextInput: View,
+    ToolbarAndroid: View,
+    ViewPagerAndroid: View,
+    DrawerLayoutAndroid: View,
+    WebView: View,
+    NativeViewGestureHandler: View,
+    TapGestureHandler: View,
+    FlingGestureHandler: View,
+    ForceTouchGestureHandler: View,
+    LongPressGestureHandler: View,
+    PanGestureHandler: View,
+    PinchGestureHandler: View,
+    RotationGestureHandler: View,
+    Directions: {},
+  };
+});
+
+jest.mock('@react-navigation/native', () => ({
+  NavigationContainer: ({ children }: { children: React.ReactNode }) => <>{children}</>,
+  useIsFocused: jest.fn().mockReturnValue(true),
+  useRoute: jest.fn().mockReturnValue({ name: 'MockedRoute' }),
+}));
+
+describe('ProfileScreen', () => {
+  it('renders correctly', () => {
+    const navigation: any = {};  // Puedes ajustar las props de navegación según sea necesario
+    const route: any = {};  // Puedes ajustar las props de ruta según sea necesario
+    const { queryAllByText } = render(
+      <NavigationContainer>
+        <ProfileScreen navigation={navigation} route={route} />
+      </NavigationContainer>
+    );
+
+    // Para "ProfileScreen Screen"
+    const perfilElements = queryAllByText(/Perfil.*/);
+    expect(perfilElements.length).toBeGreaterThan(0);
+    
+  });
+});
