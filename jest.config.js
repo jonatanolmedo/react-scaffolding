e6dc8cba8924927d952db820e2f789a8a86447da
Jonatan Olmedo <olmedojo@MacBook-Pro-3.local>
2023-10-23 11:53:25 -0300

    Code coverage

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,12 @@ module.exports = {
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  // Configura los paths para excluir de la cobertura
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/__tests__/',  // Excluir directorio de pruebas
+    '/src/example/context/hooks/MyHook/useExitBackButton.tsx',
+    '/src/example/components/screens/LoginScreen/LoginScreen.tsx',
+    '/src/example/navigators/', // Excluir directorio de navegación
+  ],  
 };
